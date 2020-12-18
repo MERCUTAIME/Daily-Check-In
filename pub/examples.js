ef3bf34c5e440785fdf8e67e122b70d92cc25158
mercutaime <ahou2000@hotmail.com>
2020-12-18 13:25:12 +0800

    removed files

--- pub/examples.js
+++
@@ -1,9 +0,0 @@
-/* JS Library usage examples */
-"use strict";
-console.log('----------')
-console.log('SCRIPT: Examples of using our libraries')
-console.log('In general, we should have the code that uses our libraries separate from the actual library code.')
-
-// dailycheckin
-const cal = $$$(jQuery)
-$(".checkin").Checkin()

2fcc41e0e240f09e76ee4710f9939b60ee92ccba
mercutaime <ahou2000@hotmail.com>
2020-12-20 06:21:36 +0800

    packaged

--- pub/dailyCheckIn.js
+++ pub/dailyCheckIn.js
@@ -4,264 +4,265 @@
 // var showsave
 // var showclose
 // var showcheck
+/* JS Library */
+"use strict";
+(function(global, document, $) { 
 
-var Checkin = function() {
+    function Checkin() {
 
-    this.defaults = {}
-    this.isChecked = false
-    this.obj = this.defaults
-    this.calendars = []
-
-}
-
-Checkin.prototype = {
-
-    init : function() {
-
-        const checkin = document.createElement('div')
-        $(checkin).addClass("checkin")
-        const body = $('body')
-        body.append(checkin)
-
-        elements = $(checkin)
-        //calendar appearance
-        this.defaults = {
-            width: 450,
-            height: 450,
-            background: '#d8efff',
-            radius: 10,
-            color: '#000000',
-            padding: 10,
-            dateArray: [3, 5, 10, 15],
-            records: {
-                3:"Hello",
-                5:"Hi"
-            }
-        }
+        this.defaults = {}
         this.isChecked = false
         this.obj = this.defaults
+        this.calendars = []
 
+    }
 
-        var _self = elements,
-            html = '',
-            currentDate = new Date(),
-            year = currentDate.getFullYear(),
-            month = currentDate.getMonth(),
-            day = currentDate.getDate(),
-            week = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+    Checkin.prototype = {
 
-            text = ''
-            switch (month +1){
-                case 1:
-                    text = 'January'
-                    break
-                case 2:
-                    text = "February"
-                    break
-                case 3:
-                    text = "March"
-                    break
-                case 4:
-                    text = "April"
-                    break
-                case 5:
-                    text = "May"
-                    break
-                case 6:
-                    text = "June"
-                    break
-                case 7:
-                    text = "July"
-                    break
-                case 8:
-                    text = "August"
-                    break
-                case 9:
-                    text = "September"
-                    break
-                case 10:
-                    text = "October"
-                    break
-                case 11:
-                    text = "November"
-                    break
-                case 12:
-                    text = "December"
-                    break
+        init: function() {
+
+            const checkin = document.createElement('div')
+            $(checkin).addClass("checkin")
+            const body = $('body')
+            $(checkin).prependTo(body);
+
+            const elements = $(checkin)
+            //calendar appearance
+            this.defaults = {
+                width: 450,
+                height: 450,
+                background: '#d8efff',
+                radius: 10,
+                color: '#000000',
+                padding: 10,
+                dateArray: [3, 5, 10, 15],
+                records: {
+                    3:"Hello",
+                    5:"Hi"
+                }
             }
-        
-        
-        _self.css({
-            width: this.obj.width + 'px',
-            height: this.obj.height,
-            background: this.obj.background,
-            borderRadius: this.obj.radius,
-            color: this.obj.color,
-            padding: this.obj.padding
-        }).append("<div class='title'><p>" + text + "</p><a class='checkBtn' href='javascript:'>Check In</a></div>")
-        $("<ul class='week'></ul><ul class='calendarList'></ul>").appendTo(_self)
-        //showself1 = _self
-        for (var i = 0; i < 7; i++) {
-            $(".week").append("<li>" + week[i] + "</li>")
-        }
-        for (var i = 0; i < 42; i++) {
-            html += "<li></li>"
-        }
-        $(".calendarList").append(html)
-        var liEle = $(".calendarList").find("li")
-        $(".week li").css({
-            width: (_self.width() / 7) + 'px',
-            height: 50 + 'px',
-            borderRight: '1px solid #000000',
-            borderBottom: '1px solid #000000',
-            boxSizing: 'border-box',
-            background: '#fff'
-        })
-        liEle.css({
-            width: (_self.width() / 7) + 'px',
-            height: 50 + 'px',
-            borderRight: '1px solid #000000',
-            borderBottom: '1px solid #000000',
-            boxSizing: 'border-box',
-            color: "#000000"
-        })
-        $(".calendarList").find("li:nth-child(7n)").css('borderRight', 'none')
-        $(".week li:nth-child(7n)").css('borderRight', 'none')
-        var firstIndent = new Date(year, month, 1).getDay()
-        //console.log("firstIndent: "+ firstIndent)
-        var lastDay = new Date(year, (month + 1), 0)
-        //console.log("lastDay: "+ lastDay)
-        var totalDay = lastDay.getDate() 
-        //console.log("totalDay: "+ totalDay)
-        for (var i = 0; i < totalDay; i++) {
-            liEle.eq(i + firstIndent).html(i + 1)
-            liEle.eq(i + firstIndent).addClass('day' + (i + 1))
-            if (Array.isArray(this.obj.dateArray)) {
-                for (var j = 0; j < this.obj.dateArray.length; j++) {
-                    if (i+1 == this.obj.dateArray[j]) {
-                        // console.log("i: "+i)
-                        // console.log("i+ firstIndent: "+(i+ firstIndent))
-                        liEle.eq(i + firstIndent).addClass('checked')
-                        liEle.eq(i + firstIndent).prop('title', this.obj.records[i+1]);                    
+            this.isChecked = false
+            this.obj = this.defaults
+
+
+            var _self = elements,
+                html = '',
+                currentDate = new Date(),
+                year = currentDate.getFullYear(),
+                month = currentDate.getMonth(),
+                day = currentDate.getDate(),
+                week = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+
+                var text = ''
+                switch (month +1){
+                    case 1:
+                        text = 'January'
+                        break
+                    case 2:
+                        text = "February"
+                        break
+                    case 3:
+                        text = "March"
+                        break
+                    case 4:
+                        text = "April"
+                        break
+                    case 5:
+                        text = "May"
+                        break
+                    case 6:
+                        text = "June"
+                        break
+                    case 7:
+                        text = "July"
+                        break
+                    case 8:
+                        text = "August"
+                        break
+                    case 9:
+                        text = "September"
+                        break
+                    case 10:
+                        text = "October"
+                        break
+                    case 11:
+                        text = "November"
+                        break
+                    case 12:
+                        text = "December"
+                        break
+                }
+            
+            
+            _self.css({
+                width: this.obj.width + 'px',
+                height: this.obj.height,
+                background: this.obj.background,
+                borderRadius: this.obj.radius,
+                color: this.obj.color,
+                padding: this.obj.padding
+            }).append("<div class='title'><p>" + text + "</p><a class='checkBtn' href='javascript:'>Check In</a></div>")
+            $("<ul class='week'></ul><ul class='calendarList'></ul>").appendTo(_self)
+            //showself1 = _self
+            for (var i = 0; i < 7; i++) {
+                $(".week").append("<li>" + week[i] + "</li>")
+            }
+            for (var i = 0; i < 42; i++) {
+                html += "<li></li>"
+            }
+            $(".calendarList").append(html)
+            var liEle = $(".calendarList").find("li")
+            $(".week li").css({
+                width: (_self.width() / 7) + 'px',
+                height: 50 + 'px',
+                borderRight: '1px solid #000000',
+                borderBottom: '1px solid #000000',
+                boxSizing: 'border-box',
+                background: '#fff'
+            })
+            liEle.css({
+                width: (_self.width() / 7) + 'px',
+                height: 50 + 'px',
+                borderRight: '1px solid #000000',
+                borderBottom: '1px solid #000000',
+                boxSizing: 'border-box',
+                color: "#000000"
+            })
+            $(".calendarList").find("li:nth-child(7n)").css('borderRight', 'none')
+            $(".week li:nth-child(7n)").css('borderRight', 'none')
+            var firstIndent = new Date(year, month, 1).getDay()
+            //console.log("firstIndent: "+ firstIndent)
+            var lastDay = new Date(year, (month + 1), 0)
+            //console.log("lastDay: "+ lastDay)
+            var totalDay = lastDay.getDate() 
+            //console.log("totalDay: "+ totalDay)
+            for (var i = 0; i < totalDay; i++) {
+                liEle.eq(i + firstIndent).html(i + 1)
+                liEle.eq(i + firstIndent).addClass('day' + (i + 1))
+                if (Array.isArray(this.obj.dateArray)) {
+                    for (var j = 0; j < this.obj.dateArray.length; j++) {
+                        if (i+1 == this.obj.dateArray[j]) {
+                            // console.log("i: "+i)
+                            // console.log("i+ firstIndent: "+(i+ firstIndent))
+                            liEle.eq(i + firstIndent).addClass('checked')
+                            liEle.eq(i + firstIndent).prop('title', this.obj.records[i+1]);                    
+                        }
                     }
                 }
             }
-        }
-    
-        $($(".day" + day)).addClass('able-checkin')
-        this.calendars.push(checkin)
         
-        
-    },
-    changeBackground: function(color){
-        for (var i=0; i<$(".checkin").length; i++ )
-            $(".checkin")[i].style.backgroundColor = color
-    },
-    changeCheckedColor: function(color){
-        for (var i=0; i<$("li.checked").length; i++ )
-            $("li.checked")[i].style.backgroundColor = color
-    },
-    changeTodayColor: function(color){
-        for (var i=0; i<$("li.able-checkin").length; i++ )
-            $("li.able-checkin")[i].style.backgroundColor = color
-    },
-    changeBtnColor: function(color){
-        for (var i=0; i<$(".title a").length; i++ )
-            $(".title a")[i].style.backgroundColor = color
-    },
-    changeCalColor: function(color){
-        var cachedThis = this
-        var oldChecked = $("li.checked")[0].style.backgroundColor
-        var oldToday = $("li.able-checkin")[0].style.backgroundColor
-        for (var i=0; i<$(".checkin li").length; i++ ){
-            $(".checkin li")[i].style.backgroundColor = color
-        }
-        cachedThis.changeCheckedColor(oldChecked)
-        cachedThis.changeTodayColor(oldToday)
-
-    },
-    modal: function() {
-        var defaults = this.defaults
-        // console.log("defaults: "+ defaults)
-        var mask = $(".mask")
-        var close = $(".closeBtn")
-        showclose = close
-        if (mask && !this.isChecked) {
-            mask.addClass('trf')
-            this.isChecked = true
-        } else {
-            return
-        }
-        close.click(function(event) {
-            event.preventDefault()
-            mask.removeClass('trf')
-        })
-        $('.checkBtn').text("Checked In")
-
-        const divform = document.createElement('div')
-        const textbox = document.createElement('form')
-        const saveBtn = document.createElement('button')
-        $(divform).addClass("form")
-		$(textbox).append("<label class='textboxTitle'>record your mood today</label> <textarea class='box'></textarea>")
-        $(divform).append(textbox)
-        $(saveBtn).addClass("saveBtn")
-        saveBtn.onclick = function(){
-            console.log("clicked save")
-            mask.removeClass('trf')
-            var day = new Date().getDate()
-            if ($(".box").val() != ""){
-                defaults.records[day] = $(".box").val()
-                $(".day"+day).prop('title', $(".box").val());     
+            $($(".day" + day)).addClass('able-checkin')
+            this.calendars.push(checkin)
+            
+            
+        },
+        changeBackground: function(color){
+            for (var i=0; i<$(".checkin").length; i++ )
+                $(".checkin")[i].style.backgroundColor = color
+        },
+        changeCheckedColor: function(color){
+            for (var i=0; i<$("li.checked").length; i++ )
+                $("li.checked")[i].style.backgroundColor = color
+        },
+        changeTodayColor: function(color){
+            for (var i=0; i<$("li.able-checkin").length; i++ )
+                $("li.able-checkin")[i].style.backgroundColor = color
+        },
+        changeBtnColor: function(color){
+            for (var i=0; i<$(".title a").length; i++ )
+                $(".title a")[i].style.backgroundColor = color
+        },
+        changeCalColor: function(color){
+            var cachedThis = this
+            var oldChecked = $("li.checked")[0].style.backgroundColor
+            var oldToday = $("li.able-checkin")[0].style.backgroundColor
+            for (var i=0; i<$(".checkin li").length; i++ ){
+                $(".checkin li")[i].style.backgroundColor = color
             }
-            console.log(defaults.records)
-        }
-        saveBtn.innerHTML = "Save"
-        $(divform).append(saveBtn)
-		const modal = $('.modal') 
-		modal.append(divform)
-        
-    },
-    events: function() {
-        var cachedThis = this    //https://stackoverflow.com/questions/9749969/in-javascript-how-can-i-call-one-prototype-method-in-another-prototype-method
-        var _self = $(".checkin")
-        var defaults = this.defaults
-        var liEle = $(".calendarList").find("li")
-        liEle.click(function(event) {
-            event.preventDefault()
-            //not checked in
-            if ($(this).hasClass('able-checkin') && !$(this).hasClass('checked')) {
-                $(this).addClass('checked')    
-                cachedThis.modal(_self)
+            cachedThis.changeCheckedColor(oldChecked)
+            cachedThis.changeTodayColor(oldToday)
+
+        },
+        modal: function() {
+            var defaults = this.defaults
+            // console.log("defaults: "+ defaults)
+            var mask = $(".mask")
+            var close = $(".closeBtn")
+            if (mask && !this.isChecked) {
+                mask.addClass('trf')
                 this.isChecked = true
-                var day = new Date().getDate()
-                if (!defaults.dateArray.includes(day))
-                    defaults.dateArray.push(day)
-                console.log(defaults.dateArray)
-            }//has checked in
-            else{
-                console.log("checked")
+            } else {
+                return
             }
-        })
-        var checkBtn = $(".checkBtn")
-        showcheck = checkBtn
-        checkBtn.click(function() {
-            if (!$('.able-checkin').hasClass('checked')) {
-                cachedThis.modal(_self)
-                $('.able-checkin').addClass('checked')
-                this.isChecked = true
+            close.click(function(event) {
+                event.preventDefault()
+                mask.removeClass('trf')
+            })
+            $('.checkBtn').text("Checked In")
+
+            const divform = document.createElement('div')
+            const textbox = document.createElement('form')
+            const saveBtn = document.createElement('button')
+            $(divform).addClass("form")
+            $(textbox).append("<label class='textboxTitle'>record your mood today</label> <textarea class='box'></textarea>")
+            $(divform).append(textbox)
+            $(saveBtn).addClass("saveBtn")
+            saveBtn.onclick = function(){
+                console.log("clicked save")
+                mask.removeClass('trf')
                 var day = new Date().getDate()
-                if (!defaults.dateArray.includes(day))
-                    defaults.dateArray.push(day)
-                console.log(defaults.dateArray)
+                if ($(".box").val() != ""){
+                    defaults.records[day] = $(".box").val()
+                    $(".day"+day).prop('title', $(".box").val());     
+                }
+                console.log(defaults.records)
             }
-            else
-                console.log("already checked")
-        })
+            saveBtn.innerHTML = "Save"
+            $(divform).append(saveBtn)
+            const modal = $('.modal') 
+            modal.append(divform)
+            
+        },
+        events: function() {
+            var cachedThis = this    //https://stackoverflow.com/questions/9749969/in-javascript-how-can-i-call-one-prototype-method-in-another-prototype-method
+            var _self = $(".checkin")
+            var defaults = this.defaults
+            var liEle = $(".calendarList").find("li")
+            liEle.click(function(event) {
+                event.preventDefault()
+                //not checked in
+                if ($(this).hasClass('able-checkin') && !$(this).hasClass('checked')) {
+                    $(this).addClass('checked')    
+                    cachedThis.modal(_self)
+                    this.isChecked = true
+                    var day = new Date().getDate()
+                    if (!defaults.dateArray.includes(day))
+                        defaults.dateArray.push(day)
+                    console.log(defaults.dateArray)
+                }//has checked in
+                else{
+                    console.log("checked")
+                }
+            })
+            const checkBtn = $(".checkBtn")
+            checkBtn.click(function() {
+                if (!$('.able-checkin').hasClass('checked')) {
+                    cachedThis.modal(_self)
+                    $('.able-checkin').addClass('checked')
+                    this.isChecked = true
+                    var day = new Date().getDate()
+                    if (!defaults.dateArray.includes(day))
+                        defaults.dateArray.push(day)
+                    console.log(defaults.dateArray)
+                }
+                else
+                    console.log("already checked")
+            })
 
-    } 
-}
+        } 
+    }
+    global.Checkin = global.Checkin || Checkin
 
-
-
+})(window, window.document, $); // pass the global window object and jquery to the anonymous function. They will now be locally scoped inside of the function.
 
 
--- pub/examples.js
+++ pub/examples.js
@@ -6,96 +6,99 @@ console.log('In general, we should have the code that uses our libraries separat
 
 // dailycheckin
 var checkin = new Checkin()
-checkin.init()
-checkin.events()
 
+function examples(){
+    checkin.init()
+    checkin.events()
 
-//https://www.w3schools.com/howto/howto_js_dropdown.asp
-var changeBackgroundBtn = $(".changeBackground")
-changeBackgroundBtn.click(function() {
-    $(".dropdown0")[0].classList.toggle("show")
-    var pink = $(".pink0")
-    pink.click(function(){
-        checkin.changeBackground('#fff4fa')
-        $(".changeColor").off()
+    //https://www.w3schools.com/howto/howto_js_dropdown.asp
+    var changeBackgroundBtn = $(".changeBackground")
+    changeBackgroundBtn.click(function() {
+        $(".dropdown0")[0].classList.toggle("show")
+        var pink = $(".pink0")
+        pink.click(function(){
+            checkin.changeBackground('#fff4fa')
+            $(".changeColor").off()
+        })
+        var purple = $(".purple0")
+        purple.click(function(){
+            checkin.changeBackground('#d7d3ff')
+        })
+        var green = $(".green0")
+        green.click(function(){
+            checkin.changeBackground('#486b4c')
+        })
     })
-    var purple = $(".purple0")
-    purple.click(function(){
-        checkin.changeBackground('#d7d3ff')
-    })
-    var green = $(".green0")
-    green.click(function(){
-        checkin.changeBackground('#486b4c')
-    })
-})
 
-var changeCheckedColorBtn = $(".changeCheckedColor")
-changeCheckedColorBtn.click(function() {
-    $(".dropdown1")[0].classList.toggle("show")
-    var pink = $(".pink1")
-    pink.click(function(){
-        checkin.changeCheckedColor('#fff4fa')
+    var changeCheckedColorBtn = $(".changeCheckedColor")
+    changeCheckedColorBtn.click(function() {
+        $(".dropdown1")[0].classList.toggle("show")
+        var pink = $(".pink1")
+        pink.click(function(){
+            checkin.changeCheckedColor('#fff4fa')
+        })
+        var purple = $(".purple1")
+        purple.click(function(){
+            checkin.changeCheckedColor('#d7d3ff')
+        })
+        var green = $(".green1")
+        green.click(function(){
+            checkin.changeCheckedColor('#486b4c')
+        })
     })
-    var purple = $(".purple1")
-    purple.click(function(){
-        checkin.changeCheckedColor('#d7d3ff')
-    })
-    var green = $(".green1")
-    green.click(function(){
-        checkin.changeCheckedColor('#486b4c')
-    })
-})
 
-var changeTodayColorBtn = $(".changeTodayColor")
-changeTodayColorBtn.click(function() {
-    $(".dropdown2")[0].classList.toggle("show")
-    var pink = $(".pink2")
-    pink.click(function(){
-        checkin.changeTodayColor('#fff4fa')
+    var changeTodayColorBtn = $(".changeTodayColor")
+    changeTodayColorBtn.click(function() {
+        $(".dropdown2")[0].classList.toggle("show")
+        var pink = $(".pink2")
+        pink.click(function(){
+            checkin.changeTodayColor('#fff4fa')
+        })
+        var purple = $(".purple2")
+        purple.click(function(){
+            checkin.changeTodayColor('#d7d3ff')
+        })
+        var green = $(".green2")
+        green.click(function(){
+            checkin.changeTodayColor('#486b4c')
+        })
     })
-    var purple = $(".purple2")
-    purple.click(function(){
-        checkin.changeTodayColor('#d7d3ff')
-    })
-    var green = $(".green2")
-    green.click(function(){
-        checkin.changeTodayColor('#486b4c')
-    })
-})
 
-var changeBtnColorBtn = $(".changeBtnColor")
-changeBtnColorBtn.click(function() {
-    $(".dropdown3")[0].classList.toggle("show")
-    var pink = $(".pink3")
-    pink.click(function(){
-        checkin.changeBtnColor('#fff4fa')
+    var changeBtnColorBtn = $(".changeBtnColor")
+    changeBtnColorBtn.click(function() {
+        $(".dropdown3")[0].classList.toggle("show")
+        var pink = $(".pink3")
+        pink.click(function(){
+            checkin.changeBtnColor('#fff4fa')
+        })
+        var purple = $(".purple3")
+        purple.click(function(){
+            checkin.changeBtnColor('#d7d3ff')
+        })
+        var green = $(".green3")
+        green.click(function(){
+            checkin.changeBtnColor('#486b4c')
+        })
     })
-    var purple = $(".purple3")
-    purple.click(function(){
-        checkin.changeBtnColor('#d7d3ff')
-    })
-    var green = $(".green3")
-    green.click(function(){
-        checkin.changeBtnColor('#486b4c')
-    })
-})
 
-var changeCalColorBtn = $(".changeCalColor")
-changeCalColorBtn.click(function() {
-    $(".dropdown4")[0].classList.toggle("show")
-    var pink = $(".pink4")
-    pink.click(function(){
-        checkin.changeCalColor('#fff4fa')
+    var changeCalColorBtn = $(".changeCalColor")
+    changeCalColorBtn.click(function() {
+        $(".dropdown4")[0].classList.toggle("show")
+        var pink = $(".pink4")
+        pink.click(function(){
+            checkin.changeCalColor('#fff4fa')
+        })
+        var purple = $(".purple4")
+        purple.click(function(){
+            checkin.changeCalColor('#d7d3ff')
+        })
+        var green = $(".green4")
+        green.click(function(){
+            checkin.changeCalColor('#486b4c')
+        })
     })
-    var purple = $(".purple4")
-    purple.click(function(){
-        checkin.changeCalColor('#d7d3ff')
-    })
-    var green = $(".green4")
-    green.click(function(){
-        checkin.changeCalColor('#486b4c')
-    })
-})
 
+}
 
+examples();
 
